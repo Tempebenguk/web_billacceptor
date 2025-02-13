555a20510b49b5d2b5697dd0f7caecbf3fa6f92b
Tempebenguk <erricksb592@gmail.com>
2025-02-13 15:12:25 +0700

    web billacceptor

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
                 .then(response => response.json())
                 .then(data => {
                     document.getElementById('total').innerText = 'Rp. ' + (data.total ? data.total.toLocaleString('id-ID') : '0');
+                    document.getElementById('received').innerText = 'Rp. ' + (data.received ? data.received.toLocaleString('id-ID') : '0');
                     let logs = data.logs.map(log => <li>${log}</li>).join('');
                     document.getElementById('logs').innerHTML = logs;
                 })
@@ -21,6 +22,7 @@
 </head>
 <body>
     <h1>Bill Acceptor Dashboard</h1>
+    <h2>Uang Masuk: <span id="received">Rp. 0</span></h2>
     <h2>Total Akumulasi: <span id="total">Rp. 0</span></h2>
     <h3>Log Transaksi:</h3>
     <ul id="logs"></ul>
