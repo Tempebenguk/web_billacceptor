36348153925a1536b3e64b7d00314214e5edc8a4
Tempebenguk <erricksb592@gmail.com>
2025-02-13 13:08:53 +0700

    web billacceptor

--- index.php
+++ index.php
@@ -1,32 +1,22 @@
-<?php
-$log_file = '/home/eksan/logs/log.txt';
-$log_content = file_get_contents($log_file);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Data Transaksi</title>
-    <meta http-equiv="refresh" content="1">
 </head>
 <body>
     <h1>Data Transaksi Bill Acceptor</h1>
-
-    <h2>Riwayat Transaksi:</h2>
-    <pre><?php echo $log_content; ?></pre>
 
     <h2>Transaksi Terkini (Real-Time):</h2>
     <div id="transaction_data"></div>
 
     <script>
         function updateTransactionData() {
-            const timestamp = new Date().getTime();
-            fetch('log-display.php?timestamp=' + timestamp)
+            fetch('transaction.php')
             .then(response => response.text())
             .then(data => {
-                document.getElementById('transaction_data').innerText = data;
+                document.getElementById('transaction_data').innerHTML = data;
             });
         }
 
--- transaction.php
+++ transaction.php
@@ -1,16 +1,12 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    
+
     $received_amount = $_POST['received_amount'] ?? 0;
     $total_amount = $_POST['total_amount'] ?? 0;
 
-    
-    $log_file = '/home/eksan/logs/log.txt';
-    $timestamp = date("Y-m-d H:i:s");
-    file_put_contents($log_file, "[$timestamp] Uang masuk: Rp. $received_amount, Total akumulasi: Rp. $total_amount\n", FILE_APPEND);
-
-    echo json_encode(["message" => "Data transaksi berhasil diterima!"]);
+    echo "Uang masuk: Rp. $received_amount<br>";
+    echo "Total Akumulasi: Rp. $total_amount<br>";
 } else {
-    echo json_encode(["message" => "Hanya menerima POST request"]);
+    echo "Hanya menerima POST request";
 }
 ?>
